99eae4b485d9f84024652c3281dc7f58206f29fe
Achmad Sofyan Pratama <fyans665@gmail.com>
2022-12-08 12:18:19 +0700

    database

---
+++ shopeeyan-APP/migrations/20221208041247-create-product.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      img: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      ShopId: {
+        type: Sequelize.INTEGER,
+        references:{
+         model: 'Shops'
+        }
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ shopeeyan-APP/models/product.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Product.belongsTo(models.Shop)
+      Product.belongsTo(models.Category)
+      Product.belongsToMany(models.Customer, {
+         
+         through: models.Cart
+      })
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    img: DataTypes.STRING,
+    description: DataTypes.STRING,
+    ShopId: DataTypes.INTEGER,
+    price: DataTypes.INTEGER,
+    CategoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
